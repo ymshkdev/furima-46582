ade152b3f2f069b56e1f5dd881f709d351481e51
ymshkdev <masahiko.yonemura@outlook.jp>
2025-11-15 18:25:14 +0900

    ActiveHash設定と出品画面へのルーティング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,38 @@ class ItemsController < ApplicationController
   def index
     @items = Item.all
   end
+
+  def new
+   @item = Item.new
+   # フォーム用にActiveHashを呼び出す
+   @categories = Category.all
+   @conditions = Condition.all
+   @shipping_fee_burdens = Shipping_fee_burden.all
+   @prefectures = Prefecture.all
+   @shipping_days = Shipping_day.all
+  end
+
+  def create
+   @item = Item.new(item_params)
+   if @item.save
+    redirect_to root_path
+   else
+    # 保存に失敗した場合もActiveHashを呼び出す
+    @categories = Category.all
+    @conditions = Condition.all
+    @shipping_fee_burdens = ShippingFeeBurden.all
+    @prefectures = Prefecture.all
+    @shipping_days = ShippingDay.all
+    render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :title, :description, :category_id, :condition_id, 
+      :shipping_fee_burden_id, :prefecture_id, :shipping_day_id, :price
+    ).merge(user_id: current_user.id)
+  end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,16 @@
 class Item < ApplicationRecord
+   belongs_to :user
+
+  # ActiveHashを使う場合のアソシエーション
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :shipping_fee_burden
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
+  # バリデーション
+  validates :title, :description, presence: true
+  validates :category_id, :condition_id, :shipping_fee_burden_id, :prefecture_id, :shipping_day_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 end
