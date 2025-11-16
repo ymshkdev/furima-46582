955c67792f39de99a6d5d6cf7308e656abf0a563
ymshkdev <masahiko.yonemura@outlook.jp>
2025-11-16 21:19:27 +0900

    ActiveHash、javascript修正

--- app/javascript/item_price.js
+++ app/javascript/item_price.js
@@ -1,23 +1,19 @@
-window.addEventListener('turbo:load', () => {
+const priceCalc = () => {
   const priceInput = document.getElementById("item-price");
+  if (!priceInput) return;
 
-  if (!priceInput) return; // 出品ページ以外でエラーにならないように
+  const addTaxDom = document.getElementById("add-tax-price");
+  const profitDom = document.getElementById("profit");
 
   priceInput.addEventListener("input", () => {
-    const price = priceInput.value;
-    const addTaxDom = document.getElementById("add-tax-price");
-    const profitDom = document.getElementById("profit");
-
-    // 数値に変換（空欄のときは0扱い）
-    const priceValue = Number(price);
-
-    // 販売手数料（10%）
+    const priceValue = Number(priceInput.value || 0);
     const tax = Math.floor(priceValue * 0.1);
-
-    // 利益
     const profit = priceValue - tax;
 
     addTaxDom.innerHTML = tax;
     profitDom.innerHTML = profit;
   });
-});
+};
+
+window.addEventListener("turbo:load", priceCalc);
+window.addEventListener("turbo:render", priceCalc);
