e606f111c7845d43e3aaa1fbf7e08f3e7913f882
ymshkdev <masahiko.yonemura@outlook.jp>
2025-11-19 21:51:31 +0900

    ログインしているユーザーのみが編集可能に

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:edit, :update]
+  before_action :authorize_user, only: [:edit, :update]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -35,15 +37,13 @@ class ItemsController < ApplicationController
   end
 
   def edit
-  @item = Item.find(params[:id])
   end
 
   def update
-   @item = Item.find(params[:id])
     if @item.update(item_params)
      redirect_to item_path(@item)
     else
-     render :edit
+     render :edit, status: :unprocessable_entity
     end
   end
 
@@ -55,4 +55,16 @@ class ItemsController < ApplicationController
       :shipping_fee_burden_id, :prefecture_id, :shipping_day_id, :price, :image
     ).merge(user_id: current_user.id)
   end
+
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
+  def authorize_user
+    unless @item.user_id == current_user.id
+      redirect_to root_path, alert: "他人の商品は編集できません"
+    end
+  end
+
+  
 end
