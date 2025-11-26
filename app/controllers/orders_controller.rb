9a8c978a090cceaa2955f69f85ee5a320b17ac42
ymshkdev <masahiko.yonemura@outlook.jp>
2025-11-26 11:11:40 +0900

    手順4 フォームの情報を保存できるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,4 +8,25 @@ class OrdersController < ApplicationController
     redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
   
+  def create
+    @order_address = OrderAddress.new(order_address_params)
+    if @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(
+      :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token
+    ).merge(user_id: current_user.id, item_id: @item.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
 end
