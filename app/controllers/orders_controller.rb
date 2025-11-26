7de9a49d31297d77862ed8867fd753475b583232
ymshkdev <masahiko.yonemura@outlook.jp>
2025-11-26 14:57:36 +0900

    手順5 エラーが表示されるようにする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,10 +10,12 @@ class OrdersController < ApplicationController
   
   def create
     @order_address = OrderAddress.new(order_address_params)
-    if @order_address.save
+    if @order_address.valid?
+      pay_item
+      @order_address.save
       redirect_to root_path
     else
-      render :index
+      render :index, status: :unprocessable_entity
     end
   end
 
