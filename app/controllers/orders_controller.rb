e882465f44281be1151d0851d69fc1fc6a35a3ed
ymshkdev <masahiko.yonemura@outlook.jp>
2025-12-01 11:55:25 +0900

    STEP2 PAY.JPを利用してクレジットカード決済ができるように設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,32 +3,43 @@ class OrdersController < ApplicationController
   before_action :authenticate_user!
   
   def index
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
     @order_address = OrderAddress.new
     # 出品者は購入させない＆売却済みはアクセス不可
     redirect_to root_path if current_user.id == @item.user_id || @item.order.present?
   end
   
   def create
-    @order_address = OrderAddress.new(order_address_params)
+    @order_address = OrderAddress.new(order_params)
     if @order_address.valid?
       pay_item
       @order_address.save
       redirect_to root_path
     else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
       render :index, status: :unprocessable_entity
     end
   end
 
   private
 
-  def order_address_params
+  def order_params
     params.require(:order_address).permit(
       :postal_code, :prefecture_id, :city, :address, :building, :phone_number, :token
-    ).merge(user_id: current_user.id, item_id: @item.id)
+    ).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
   end
 
   def set_item
     @item = Item.find(params[:item_id])
   end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # PAY.JPテスト秘密鍵
+    Payjp::Charge.create(
+     amount: @item.price,  # 商品の値段
+     card: order_params[:token],    # カードトークン
+     currency: 'jpy'                 # 通貨の種類（日本円）
+     )
+  end  
 
 end
