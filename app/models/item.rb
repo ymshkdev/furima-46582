237a3d0b43d898fa5e3f81ce6971ae1fb66409d0
ymshkdev <masahiko.yonemura@outlook.jp>
2025-12-01 17:16:21 +0900

    売却済みの商品に画像上に「sold out」を表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -16,4 +16,8 @@ class Item < ApplicationRecord
   validates :price, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
 
   has_one :order
+
+  def sold_out?
+  order.present?
+  end
 end
