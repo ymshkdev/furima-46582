a8ed8e559ade5246065f69ac9f12674a3ffbe48d
ymshkdev <masahiko.yonemura@outlook.jp>
2025-12-02 07:43:29 +0900

    バリデーションチェック追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -4,10 +4,11 @@ class OrderAddress
 
   # バリデーションの処理
   with_options presence: true do
-    validates :postal_code
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/, message: "is invalid" }
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :address
-    validates :phone_number
+    validates :phone_number, format: { with: /\A\d{10,11}\z/, message: "is invalid" }
     validates :token
     validates :user_id
     validates :item_id
